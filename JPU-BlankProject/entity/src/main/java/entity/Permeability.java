bd0b3ddf2dd1d4a8a28e6eea25d2cc0320d0d6b6
Bruno <Bruno@DESKTOP-Q6RQC2L>
2019-05-24 15:29:46 +0200

    Code bouchonné pour Sprite [package entity]. Création de l'énumération Permeability [package entity]

---
+++ JPU-BlankProject/entity/src/main/java/entity/Permeability.java
@@ -0,0 +1,14 @@
+package entity;
+
+/**
+ * The enum Permeability
+ * 
+ * @author Bruno
+ */
+
+public enum Permeability {
+	
+	BLOCKING,
+	UNBLOCKING
+	
+}
---
+++ JPU-BlankProject/entity/src/main/java/entity/Sprite.java
@@ -0,0 +1,123 @@
+package entity;
+
+import java.awt.Image;
+
+/**
+ * <h1>Sprite class<h1>
+ * 
+ * @author Bruno
+ *
+ */
+
+public class Sprite {
+	
+	/** The image of the sprite.*/
+	private Image image;
+	
+	/** The image name to find it.*/
+	private String imageName;
+	
+	/** The image on the console.*/
+	private char consoleImage;
+	
+	/** Is the image loaded ?*/
+	private boolean imageLoaded;
+	
+	/**
+	 * Instanciate a new sprite.
+	 * 
+	 * @param character
+	 * 				The character on the console
+	 * @param imageName
+	 * 				The image name which indicates which image needs to be loaded
+	 */
+	public Sprite(final char character, final String imageName) {
+		
+	}
+	
+	/**
+	 * Instanciate a new sprite.
+	 * 
+	 * @param character
+	 * 				The character on the console
+	 */
+	public Sprite(final char character) {
+		
+	}
+	
+	/**
+	 * Returns back the image of the designed sprite.
+	 * 
+	 * @return Image
+	 * 			The image of the sprite
+	 */
+	public final Image getImage() {
+		return image;
+	}
+	
+	/**
+	 *  Set the image of the designed sprite
+	 *  
+	 *  @param image
+	 *  		The image to print
+	 */
+	public void setImage(final Image image) {
+		
+	}
+	
+	/**
+	 * 	Get the name of the image
+	 * @return imageName
+	 */
+	public final String getImageName() {
+		return "";
+	}
+	
+	/**
+	 * Set the image name
+	 * @param imageName
+	 */
+	public void setImageName(String imageName) {
+		
+	}
+	
+	/**
+	 * Get the image of the object in the console
+	 * @return consoleImage
+	 */
+	public final char getConsoleImage() {
+		return ' ';
+	}
+	
+	/**
+	 * Set the image of the sprite in the console
+	 * @param consoleImage
+	 */
+	public final void setConsoleImage(final char consoleImage) {
+		
+	}
+	
+	/**
+	 * Check if the image of the sprite is loaded
+	 * @return boolean
+	 */
+	public boolean isImageLoaded() {
+		return true;
+	}
+	
+	/**
+	 * Set is the image is loaded or not
+	 * @param isImageLoaded
+	 */
+	public void setImageLoaded(final boolean isImageLoaded) {
+		
+	}
+	
+	/**
+	 * Load the image in the sprite
+	 */
+	public void loadImage() {
+		
+	}
+	
+}
