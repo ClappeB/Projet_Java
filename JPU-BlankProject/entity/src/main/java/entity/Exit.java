20e3a39803cee9a210042289ba756efa5ea7c45f
loren <loren@DESKTOP-4G4RBLB>
2019-05-27 13:06:00 +0200

    new name images

--- JPU-BlankProject/entity/src/main/java/entity/Exit.java
+++ JPU-BlankProject/entity/src/main/java/entity/Exit.java
@@ -2,8 +2,8 @@ package entity;
 
 public class Exit extends MotionLessElements {
 
-	private static final Sprite SPRITE = new Sprite('e', "mur incassable.png");
-	private Sprite EXIT = new Sprite('e', "Porte.png");
+	private static final Sprite SPRITE = new Sprite('e', ".png");
+	private Sprite EXIT = new Sprite('e', "exit.png");
 			
 	Exit() {
 		super(SPRITE, Permeability.BLOCKING);
