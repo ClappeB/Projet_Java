686648e3c3619da3944703f032ee4c97749b09c7
loren <loren@DESKTOP-4G4RBLB>
2019-05-27 13:19:38 +0200

    BackgrounDirt

--- JPU-BlankProject/entity/src/main/java/entity/BackgroundDirt.java
+++ JPU-BlankProject/entity/src/main/java/entity/BackgroundDirt.java
@@ -2,7 +2,9 @@ package entity;
 
 public class BackgroundDirt extends MotionLessElements {
 
+	private static final Sprite SPRITE = new Sprite('B', "backgroundDirt.png");
+	
 	BackgroundDirt() {
-		
+		super(SPRITE, Permeability.UNBLOCKING);
 	}
 }
