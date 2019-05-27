434843514f9bde5cd3045e59def0352789f4afee
Bruno <bruno.clappe@viacesi.fr>
2019-05-27 13:54:01 +0200

    Exit review.

--- JPU-BlankProject/entity/src/main/java/entity/Exit.java
+++ JPU-BlankProject/entity/src/main/java/entity/Exit.java
@@ -2,15 +2,15 @@ package entity;
 
 public class Exit extends MotionLessElements {
 
-	private static final Sprite SPRITE = new Sprite('e', "unbreakableBlock.png");
-	private Sprite EXIT = new Sprite('e', "exit.png");
+	private static final Sprite SPRITE = new Sprite('E', "unbreakableBlock.png");
+	private Sprite EXIT = new Sprite('E', "exit.png");
 			
 	Exit() {
 		super(SPRITE, Permeability.BLOCKING);
 	}
 	
-	public void diamondCheck() {
-		if(Player.getDiamondNumber() <= 5) {
+	public void diamondCheck(int diamondNumber) {
+		if(diamondNumber >= 5) {
 			this.setSprite(EXIT);
 			this.setPermeability(Permeability.UNBLOCKING);
 		}
