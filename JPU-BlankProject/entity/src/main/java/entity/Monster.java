4082f19ab9fc8afb25965a857b5ce721dacea4fc
Bruno <bruno.clappe@viacesi.fr>
2019-05-27 14:07:19 +0200

    Implementing monster

--- JPU-BlankProject/entity/src/main/java/entity/Monster.java
+++ JPU-BlankProject/entity/src/main/java/entity/Monster.java
@@ -2,8 +2,10 @@ package entity;
 
 public class Monster extends Killable implements IKill{
 	
+	private static Sprite sprite = new Sprite('M', "Image jeu/Monstres/bat.png");
+	
 	Monster() {
-		
+		super(sprite, Permeability.UNBLOCKING);
 	}
 	
 	public void diamondExplosion() {
