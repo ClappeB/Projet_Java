7b0ab9f48d17be5f42daa6b7a787ba2427b9fa9d
Gaultier <Gaultier@DESKTOP-S8J9BRI>
2019-06-01 15:20:40 +0200

    Test View Panel

---
+++ JPU-BlankProject/view/src/test/java/view/ViewPanelTest.java
@@ -0,0 +1,65 @@
+package view;
+
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Observable;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import contract.IModel;
+import entity.Map;
+import entity.Sprite;
+
+public class ViewPanelTest {
+
+    private static IModel model;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        model = new fakeModel();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testViewPanel() {
+        ViewPanel vp = new ViewPanel(new ViewFrame(model));
+        assertNotNull("View Panel shouldn't be null.", vp);
+    }
+
+    @Test
+    public void testUpdateObservableObject() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPaintComponentGraphics() {
+//    	assertTrue("Should return a sprite.", getImage. );
+    }
+
+}
+
+class fakeModel implements IModel {
+
+    @Override
+    public Map getMap() {
+        return null;
+    }
+
+    @Override
+    public void loadMap() {
+
+    }
+
+    @Override
+    public Observable getObservable() {
+        return new Observable();
+    }
+
+}
