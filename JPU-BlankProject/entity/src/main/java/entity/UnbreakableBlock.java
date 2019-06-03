a6a7efaeed8c2c8f1a2fe42440c5be4d6236ac43
MoGev <50356285+MoGev@users.noreply.github.com>
2019-06-03 11:45:26 +0200

    Entity Javadoc

--- JPU-BlankProject/entity/src/main/java/entity/UnbreakableBlock.java
+++ JPU-BlankProject/entity/src/main/java/entity/UnbreakableBlock.java
@@ -1,9 +1,13 @@
 package entity;
-
+/**
+ * The class UnbreakableBlocks
+ */
 public class UnbreakableBlock extends MotionLessElements {
 
+	/** Sprite */
 	private static final Sprite SPRITE = new Sprite('U', "unbreakableBlock.png");
 	
+	/** Sprite */
 	UnbreakableBlock() {
 		super(SPRITE, Permeability.BLOCKING);
 	}
